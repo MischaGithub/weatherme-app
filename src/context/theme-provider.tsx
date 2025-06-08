83a5df789ab4a199258f11d4eb081e7ef9e2f5b9
Mischa Achmat <mischaachmat@Mischas-MacBook-Air.local>
2025-06-08 17:48:12 +0200

    add tanstack query client to handle state and caching location

---
+++ src/context/theme-provider.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { createContext, useContext, useEffect, useState } from "react";
+
+type Theme = "dark" | "light" | "system";
+
+type ThemeProviderProps = {
+  children: React.ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
+};
+
+type ThemeProviderState = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+const initialState: ThemeProviderState = {
+  theme: "system",
+  setTheme: () => null,
+};
+
+const ThemeProviderContext = createContext<ThemeProviderState>(initialState);
+
+export function ThemeProvider({
+  children,
+  defaultTheme = "system",
+  storageKey = "theme",
+}: ThemeProviderProps) {
+  const [theme, setThemeState] = useState<Theme>(defaultTheme);
+  const [isMounted, setIsMounted] = useState(false); // Track client-side mount
+
+  useEffect(() => {
+    setIsMounted(true);
+
+    const storedTheme = localStorage.getItem(storageKey) as Theme;
+    if (
+      storedTheme === "light" ||
+      storedTheme === "dark" ||
+      storedTheme === "system"
+    ) {
+      setThemeState(storedTheme);
+    }
+  }, [storageKey]);
+
+  useEffect(() => {
+    if (!isMounted) return;
+
+    const root = document.documentElement;
+    root.classList.remove("light", "dark");
+
+    const applyTheme = (t: Theme) => {
+      if (t === "system") {
+        const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+          .matches
+          ? "dark"
+          : "light";
+        root.classList.add(systemTheme);
+      } else {
+        root.classList.add(t);
+      }
+    };
+
+    applyTheme(theme);
+  }, [theme, isMounted]);
+
+  const setTheme = (newTheme: Theme) => {
+    localStorage.setItem(storageKey, newTheme);
+    setThemeState(newTheme);
+  };
+
+  const value: ThemeProviderState = {
+    theme,
+    setTheme,
+  };
+
+  return (
+    <ThemeProviderContext.Provider value={value}>
+      {children}
+    </ThemeProviderContext.Provider>
+  );
+}
+
+export const useTheme = () => {
+  const context = useContext(ThemeProviderContext);
+  if (!context) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return context;
+};
